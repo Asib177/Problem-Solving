1bee3f7036363492a932a160c9a2be26171f55c9
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-08-17 12:19:40 +0600

    Add files via upload

---
+++ 1332. Remove Palindromic Subsequences(LeetCode)/solve.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int left = 0, right = s.length()-1;
+        while(left<right){
+            if(s[left]!=s[right]) return 2;
+            left++;
+            right--;
+        }
+        return 1;
+    }
+};
